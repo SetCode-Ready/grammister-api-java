0828c365f2a61807a06898cc5aada8538a79e55b
kauecdev <kaue_cavalcante.cnt@outlook.com>
2021-12-16 23:22:14 -0300

    feat: Added web api configuration strings

---
+++ src/main/java/set/code/ready/grammisterapi/GrammisterApiApplication.java
@@ -0,0 +1,13 @@
+package set.code.ready.grammisterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrammisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrammisterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/set/code/ready/grammisterapi/GrammisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package set.code.ready.grammisterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrammisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
