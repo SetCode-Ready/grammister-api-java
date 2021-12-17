1ee55db0426bd55bc1431f95fa8d685031ab4846
kauecdev <kaue_cavalcante.cnt@outlook.com>
2021-12-16 23:20:18 -0300

    feat: Created TokenService and UserDetailsServiceImpl

---
+++ src/main/java/set/code/ready/grammisterapi/services/TokenService.java
@@ -0,0 +1,53 @@
+package set.code.ready.grammisterapi.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import set.code.ready.grammisterapi.model.User;
+import set.code.ready.grammisterapi.model.UserDetailsImpl;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${jwt.expiration}")
+    private String expiration;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(Authentication authentication) {
+
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("grammister")
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        Claims body = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return body.getSubject();
+    }
+
+}
